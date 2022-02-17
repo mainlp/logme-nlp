7696b18fa14377f9bfcff1f91cd1de53db03013b
Max Müller-Eberstein <personads@users.noreply.github.com>
2022-02-17 15:29:32 +0100

    added prediction and evaluation to bash scripts

--- tasks/deprel/run_classification.sh
+++ tasks/deprel/run_classification.sh
@@ -12,27 +12,52 @@ SEEDS=( 4012 5060 8823 8857 9908 )
 for rsd_idx in "${!SEEDS[@]}"; do
   # iterate over encoders
   for enc_idx in "${!ENCODERS[@]}"; do
-    echo "Training ${CLASSIFIER}-classifier using '${ENCODERS[$enc_idx]}' and random seed ${SEEDS[$rsd_idx]}:"
+    echo "Experiment: '${ENCODERS[$enc_idx]}' and random seed ${SEEDS[$rsd_idx]}."
 
     exp_dir=$EXP_PATH/model${enc_idx}-${CLASSIFIER}-rs${SEEDS[$rsd_idx]}
     # check if experiment already exists
-    if [ -d "$exp_dir" ]; then
-      echo "[Warning] Experiment '$exp_dir' already exists. Skipped."
-      continue
+    if [ -f "$exp_dir/best.pt" ]; then
+      echo "[Warning] Experiment '$exp_dir' already exists. Not retraining."
+    # if experiment is new, train classifier
+    else
+      echo "Training ${CLASSIFIER}-classifier using '${ENCODERS[$enc_idx]}' and random seed ${SEEDS[$rsd_idx]}."
+      # train classifier
+      python ../../classify.py \
+        --task "token_classification" \
+        --train_path $DATA_PATH/en-ewt-train.csv \
+        --test_path $DATA_PATH/en-ewt-dev.csv \
+        --exp_path ${exp_dir} \
+        --embedding_model ${EMB_TYPE}:${ENCODERS[$enc_idx]} \
+        --classifier ${CLASSIFIER} \
+        --seed ${SEEDS[$rsd_idx]}
+
+      # save experiment info
+      echo "${EMB_TYPE}:${ENCODERS[$enc_idx]} -> ${CLASSIFIER} with RS=${SEEDS[$rsd_idx]}" > $exp_dir/experiment-info.txt
     fi
 
-    # train classifier
-    python ../../classify.py \
-      --task "token_classification" \
-      --train_path $DATA_PATH/en-ewt-train.csv \
-      --test_path $DATA_PATH/en-ewt-dev.csv \
-      --exp_path ${exp_dir} \
-      --embedding_model ${EMB_TYPE}:${ENCODERS[$enc_idx]} \
-      --classifier ${CLASSIFIER} \
-      --seed ${SEEDS[$rsd_idx]}
+    # check if prediction already exists
+    if [ -f "$exp_dir/en-ewt-dev-pred.csv" ]; then
+      echo "[Warning] Prediction '$exp_dir/en-ewt-dev-pred.csv' already exists. Not re-predicting."
+    # if no prediction is available, run inference
+    else
+      # run prediction
+      python ../../classify.py \
+        --task "token_classification" \
+        --train_path $DATA_PATH/en-ewt-train.csv \
+        --test_path $DATA_PATH/en-ewt-dev.csv \
+        --exp_path ${exp_dir} \
+        --embedding_model ${EMB_TYPE}:${ENCODERS[$enc_idx]} \
+        --classifier ${CLASSIFIER} \
+        --seed ${SEEDS[$rsd_idx]} \
+        --prediction_only
+    fi
 
-    # save experiment info
-    echo "${EMB_TYPE}:${ENCODERS[$enc_idx]} -> ${CLASSIFIER} with RS=${SEEDS[$rsd_idx]}" > $exp_dir/experiment-info.txt
+    # run evaluation
+    python ../../evaluate.py \
+      --gold_path ${DATA_PATH}/en-ewt-dev.csv \
+      --pred_path ${exp_dir}/en-ewt-dev-pred.csv \
+      --out_path ${exp_dir}
+
     echo
   done
 done
